babe0e2da178e32f1ca95c7f49a6a810477842e7
CeeJ01 <34404129+CeeJ01@users.noreply.github.com>
2018-01-20 13:06:31 -0700

    Add files via upload
    Finished Exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-var palindromes = function() {
+var palindromes = function(string) {
+	string = string.toLowerCase().replace(/[!&\/\\#,+()$~%.'":*?<>{}]/g,'');
 
+	for (var i = 0; i < string.length / 2; i++) {
+		if (string[i] != string[(string.length - 1) - i]){
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 }
-
-module.exports = palindromes
+module.exports = palindromes;
